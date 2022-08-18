c18346ff34cb3928654ab980a1fbe8e79db6702c
anastasiia <anastasiya1155@gmail.com>
2022-08-18 12:17:29 +0800

    initial commit

---
+++ src/components/BoxAlert/index.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { FaInfoCircle } from "react-icons/fa";
+
+import styles from './index.module.css'
+
+export function BoxAlert({ type }) {
+  return (
+    <div className={`${styles.boxAlert} ${styles[type]}`}>
+      {type === 'empty' && (
+        <>
+          <FaInfoCircle size={36} />
+          <p>The list is empty</p>
+        </>
+      )}
+
+      {type === 'warning' && (
+        <>
+          <FaInfoCircle size={36} />
+          <p>
+            Could not find any tasks,
+            try again with another term.
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
